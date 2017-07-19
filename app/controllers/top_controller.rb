5fd605661f1427fb9318b6b1a1509546938421b2
conol <conol@mac-book-air.local>
2017-07-19 19:57:37 +0900

    slack通知

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,4 +2,11 @@ class TopController < ApplicationController
 
   def index
   end
+
+  def slack
+    slack = Slack::Incoming::Webhooks.new "https://hooks.slack.com/services/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"
+    slack.post "alert from rails!"
+
+    redirect_to :root
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
 
   root "top#index"
+  post "slack" => "top#slack"
 end
