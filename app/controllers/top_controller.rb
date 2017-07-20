688ad6decbb3ec8107b65e37fff6126b01fd1731
conol <conol@mac-book-air.local>
2017-07-21 00:59:37 +0900

    レスポンスのjson少し編集

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -5,10 +5,9 @@ class TopController < ApplicationController
 
   def slack
     slack = Slack::Incoming::Webhooks.new "https://hooks.slack.com/services/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"
-    slack.post "alert from rails!"
+    # slack.post "alert from rails!"
 
-    success = [status: "OK!"]
+    success = {status: "OK!"}
     render json: success
-
   end
 end
