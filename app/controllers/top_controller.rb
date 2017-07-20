8959177505d7840ab6eb5f6da9d24f1d801b1bca
conol <conol@mac-book-air.local>
2017-07-21 01:34:21 +0900

    アプリ名変更して作り直しかんりょう

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -6,8 +6,7 @@ class TopController < ApplicationController
   def slack
     slack = Slack::Incoming::Webhooks.new "https://hooks.slack.com/services/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"
     # slack.post "alert from rails!"
-
-    success = {status: "OK!"}
+    success = {status: "今は動作停止させてます！"}
     render json: success
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-
   root "top#index"
   get  "slack" => "top#slack"
 end
