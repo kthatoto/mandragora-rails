09e236a5252e6e61e9c71b48c3053fcfaadb8fa1
tk1to <tk02dr@gmail.com>
2017-07-30 23:55:46 +0900

    slack通知

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -15,7 +15,7 @@ class ApiController < ApplicationController
   def slack
     slack = Slack::Incoming::Webhooks.new "https://hooks.slack.com/services/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"
     slack.post "alert from rails!"
-    success = {status: "今は動作停止させてます！"}
+    success = {status: "alerted!"}
     render json: success
   end
 
